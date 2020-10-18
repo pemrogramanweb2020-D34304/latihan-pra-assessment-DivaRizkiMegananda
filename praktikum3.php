8904798776a76d5ebff8dc8f7d1f2b8881c5ca10
Diva Rizki M <71258418+DivaRizkiMegananda@users.noreply.github.com>
2020-10-19 02:41:54 +0700

    update foreach praktikum3

--- praktikum3.php
+++ praktikum3.php
@@ -1,92 +1,125 @@
 <?php
-
 $buku = array(
-      array(
-            'judul' => 'Sherlock Holmes',
-            'pengarang' => 'Arthur Cona Doyle',
-            'harga' => 50000,
-            'stok' => 20
-      ),
-      array(
-            'judul' => 'Arsene Lupin',
-            'pengarang' => 'Maurice LeBlanc',
-            'harga' => 250000,
-            'stok' => 50
-      ),
-      array(
-            'judul' => 'Games Of Throne',
-            'pengarang' => 'George R.R.Martin',
-            'harga' => 125000,
-            'stok' => 60
-      ),
+  array(
+    'judul' => 'Sherlock Holmes',
+    'pengarang' => 'Arthur Cona Doyle',
+    'harga' => 50000,
+    'stok' => 20
+  ),
+  array(
+    'judul' => 'Arsene Lupin',
+    'pengarang' => 'Maurice LeBlanc',
+    'harga' => 250000,
+    'stok' => 50
+  ),
+  array(
+    'judul' => 'Games Of Throne',
+    'pengarang' => 'George R.R.Martin',
+    'harga' => 125000,
+    'stok' => 60
+  ),
 );
 
-echo "<table border='1' cellpadding='5'>";
-echo "<thead>
-          <th colspan='4'><b>DATA BUKU</b></th>
-          </thead>
-          <thead>
-          <th><b>Judul</b></th>
-          <th><b>Pengarang</b></th>
-          <th><b>Harga (Rp)</b></th>
-          <th><b>Stok</b></th>
-          </thead>";
-
-for ($j = 0; $j < count($buku); $j++) {
-      echo "<tr>";
-      echo "<td>" . $buku[$j]['judul'] . "</td>";
-      echo "<td>" . $buku[$j]['pengarang'] . "</td>";
-      echo "<td>" . $buku[$j]['harga'] . "</td>";
-      echo "<td>" . $buku[$j]['stok'] . "</td>";
-      echo "</tr>";
-}
-
-echo "</table>";
-echo "<table border=0 cellpadding=8>
-          <tr><td></td></tr>   
-          </table>";
-
-$beli = array(
-      array(
-            'judul' => 'Sherlock Holmes',
-            'jumlah' => 5
-      ),
-      array(
-            'judul' => 'Arsene Lupin',
-            'jumlah' => 2
-      ),
-      array(
-            'judul' => 'Game Of Throne',
-            'jumlah' => 4
-      ),
+$pembelian = array(
+  array(
+    'judul' => 'Sherlock Holmes',
+    'jumlah' => '5'
+  ),
+  array(
+    'judul' => 'Arsene Lupin',
+    'jumlah' => '2'
+  ),
+  array(
+    'judul' => 'Game Of Throne',
+    'jumlah' => '4'
+  ),
 );
 $total = 0;
-echo "<table border='1' cellpadding='5'>";
-echo "<thead>
-          <th colspan='3'><b>Data Pembelian</b></th>
-          </thead>
-          <thead>
-          <th><b>Judul</b></th>
-          <th><b>Jumlah<b></th>
-          <th><b>Harga (Rp)</b></th>
-          </thead>";
+?>
 
-for ($j = 0; $j < count($beli); $j++) {
-      echo "<tr>";
-      echo "<td>" . $beli[$j]['judul'] . "</td>";
-      echo "<td>" . $beli[$j]['jumlah'] . "</td>";
-      for ($y = 0; $y < count($buku); $y++) {
-            if ($beli[$j]['judul'] == $buku[$y]['judul']) {
-                  $harga = $buku[$y]['harga'] * $beli[$j]['jumlah'];
-                  break;
-            }
-      }
-      $total = $total + $harga;
-      echo "<td>" . $harga . "</td>";
-      echo "</tr>";
-}
-echo "<tr>
-          <td colspan='2'><b>Total Harga (Rp)</b></td>
-          <td>" . $total . "</td>
-          </tr>";
-echo "</table>";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+
+  </style>
+</head>
+
+<body>
+
+  <table border='1' cellpadding='5'>
+    <thead>
+      <th colspan='4'><b>DATA BUKU</b></th>
+    </thead>
+    <thead>
+      <th><b>Judul</b></th>
+      <th><b>Pengarang</b></th>
+      <th><b>Harga (Rp)</b></th>
+      <th><b>Stok</b></th>
+    </thead>
+
+    <?php foreach ($buku as $book) { ?>
+      <tr>
+        <td> <?= $book['judul']; ?></td>
+        <td> <?= $book['pengarang']; ?></td>
+        <td> <?= $book['harga']; ?></td>
+        <td> <?= $book['stok']; ?></td>
+      </tr>
+    <?php } ?>
+
+  </table>
+  <br>
+  <table border="1" cellpadding='5'>
+    <thead>
+      <th colspan="4"><b> Data Pembelian</b></th>
+    </thead>
+    <thead>
+      <th>Judul</th>
+      <th>Jumlah</th>
+      <th>Harga</th>
+    </thead>
+
+
+    <?php foreach ($pembelian as $beli) { ?>
+
+
+      <tr>
+        <td>
+          <?= $beli['judul']; ?>
+        </td>
+        <td>
+          <?= $beli['jumlah']; ?>
+        </td>
+
+        <?php foreach ($buku as $bookk) { ?>
+          <?php
+          if ($beli['judul'] == $bookk['judul']) {
+            $harga = $bookk['harga'] * $beli['jumlah'];
+            break;
+          }
+          $total = $total + $harga;
+          ?>
+        <?php } ?>
+
+        <td>
+          <?= $harga; ?>
+        </td>
+      </tr>
+
+    <?php } ?>
+
+    <td colspan='2'>
+      <b> Total Harga</b>
+    </td>
+    <td>
+      <?= $total; ?>
+    </td>
+
+  </table>
+</body>
+
+</html>
